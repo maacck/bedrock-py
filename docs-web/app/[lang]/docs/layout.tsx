1ba1ad69137af004e8ca37d4de7007e4e47f12a7
maacck <c.mai@madainchina.com>
2026-05-08 15:06:20 +0800

    feat: add ai chat to doc

--- docs-web/app/[lang]/docs/layout.tsx
+++ docs-web/app/[lang]/docs/layout.tsx
@@ -1,7 +1,16 @@
-import { source } from '@/lib/source';
-import { DocsLayout } from 'fumadocs-ui/layouts/notebook';
-import { baseOptions } from '@/lib/layout.shared';
-
+import { source } from "@/lib/source";
+import { DocsLayout } from "fumadocs-ui/layouts/notebook";
+import { baseOptions } from "@/lib/layout.shared";
+// import the installed components, e.g.
+import {
+  AISearch,
+  AISearchPanel,
+  AISearchTrigger,
+} from "@/components/ai/search";
+import { MessageCircleIcon } from "lucide-react";
+// or import your own button styles
+import { buttonVariants } from "fumadocs-ui/components/ui/button";
+import { cn } from "@/lib/utils";
 export default async function Layout({
   params,
   children,
@@ -13,6 +22,21 @@ export default async function Layout({
 
   return (
     <DocsLayout tree={source.getPageTree(lang)} {...baseOptions(lang)}>
+      <AISearch>
+        <AISearchPanel />
+        <AISearchTrigger
+          position="float"
+          className={cn(
+            buttonVariants({
+              variant: "secondary",
+              className: "text-fd-muted-foreground rounded-2xl",
+            }),
+          )}
+        >
+          <MessageCircleIcon className="size-4.5" />
+          Ask AI
+        </AISearchTrigger>
+      </AISearch>
       {children}
     </DocsLayout>
   );
